408278860cd32ba4ca0c24b506db9dd0941b7610
Polina Pavlenko <polina.pavvlenko@gmail.com>
2025-06-12 20:14:39 +0300

    Navbar built

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": "1",
     },
   },
 ];
